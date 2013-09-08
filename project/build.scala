19872ae19a4a244e6af2cb2a610782f46a2b3efe
Akira Ueda <ue@gol.com>
2013-09-09 02:40:31 +0900

    bump version

--- project/build.scala
+++ project/build.scala
@@ -5,7 +5,7 @@ object MyBuild extends Build {
 
   lazy val main = Project("shirahae-sql", file("."),
     settings = Defaults.defaultSettings ++ Seq(
-      version := "0.7",
+      version := "0.8",
       organization := "net.physalis",
       crossScalaVersions := Seq("2.10.2"),
       scalaVersion := "2.10.2",
